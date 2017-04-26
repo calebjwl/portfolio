7e7ed2e527ed39e51fab14d69f31d044c5366704
Caleb Lee <caleb.gwl@gmail.com>
2017-04-26 11:08:32 -0500

    Mockup of home and about page

--- app/router.js
+++ app/router.js
@@ -7,7 +7,7 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('home');
+  this.route('home', { path: '/' });
   this.route('about');
   this.route('contact');
   this.route('projects');

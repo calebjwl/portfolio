8bb5ded69f4c3f77f4e3362178efd7348f911af6
Caleb Lee <caleb.gwl@gmail.com>
2017-05-24 15:08:51 -0500

    Redesign draft 1

--- app/transitions.js
+++ app/transitions.js
@@ -1,7 +1,13 @@
 export default function(){
   this.transition(
-    this.fromRoute('home'),
+    // this.fromRoute('home'),
     this.toRoute('about'),
+    this.use('fade'),
+    this.reverse('toRight')
+  )
+  this.transition(
+    // this.fromRoute('home'),
+    this.toRoute('home'),
     this.use('toLeft'),
     this.reverse('toRight')
   )

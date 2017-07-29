aa824e3d29b62db8c512da59f91e58f85671498e
Caleb Lee <caleb.gwl@gmail.com>
2017-07-28 21:06:07 -0500

    Re-order portfolio

--- app/projects/web/controller.js
+++ app/projects/web/controller.js
@@ -35,20 +35,20 @@ export default Ember.Controller.extend({
     //   github: 'https://github.com/calebjwl/16-spotify-collection'
     // },
     {
+      src: '/img/my-contacts.png',
+      title: 'MyContacts',
+      description: 'MyContacts is a CRUD (Create, Read, Update, Destroy) app I built using vanilla Javascript. Users can Create new contacts, Read the contacts as displayed in their contact list, as well as Update and Destroy their created contacts. Try it yourself!',
+      tools: 'Javascript | HTML | CSS',
+      link: 'http://my-contacts.surge.sh/',
+      github: 'https://github.com/calebjwl/15-contact-manager'
+    },
+    {
       src: '/img/wave-rider.png',
       title: 'Wave Rider',
       description: 'Wave Rider Surf Blog serves as a mockup design template for a business website and blog, featuring a banner, article body section, and interactive footer. I wanted to create a simple and clean layout that had a consistent color palette, highlighting the sunset tones of the banner image.',
       tools: 'HTML | CSS',
       link: 'http://waverider.surge.sh',
       github: 'https://github.com/calebjwl/06-surf-and-paddle'
-    },
-    {
-      src: '/img/my-contacts.png',
-      title: 'MyContacts',
-      description: 'MyContacts is a CRUD (Create, Read, Update, Destroy) app I built using vanilla Javascript. Users can Create new contacts, Read the contacts as displayed in their contact list, as well as Update and Destroy their created contacts. Try it yourself!',
-      tools: 'HTML | CSS | Javascript',
-      link: 'http://my-contacts.surge.sh/',
-      github: 'https://github.com/calebjwl/15-contact-manager'
     },
   ]
 });
